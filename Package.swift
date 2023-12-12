47161af06f9bc56914e0dd701f7cab26c92e8f55
Anand Bhavsar <anand.bhavsar@spec-india.com>
2023-12-12 15:37:41 +0530

    add swift package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "ApiPackageManager",
+    name: "ApiManagerPackage",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ApiPackageManager",
-            targets: ["ApiPackageManager"]),
+            name: "ApiManagerPackage",
+            targets: ["ApiManagerPackage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ApiPackageManager"),
-        .testTarget(
-            name: "ApiPackageManagerTests",
-            dependencies: ["ApiPackageManager"]),
+            name: "ApiManagerPackage"),
     ]
 )
